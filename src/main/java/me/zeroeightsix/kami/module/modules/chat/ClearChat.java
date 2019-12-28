23feeaad7c62eaac2ad8a70e48d7b8f9c1a99679
FINZ0 <55377447+FINZ0@users.noreply.github.com>
2019-12-28 13:22:20 +0100

    Create ClearChat.java

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/ClearChat.java
@@ -0,0 +1,8 @@
+package me.zeroeightsix.kami.module.modules.chat;
+
+import me.zeroeightsix.kami.module.Module;
+
+@Module.Info(name = "ClearChat", category = Module.Category.CHAT)
+public class ClickGUI extends Module {
+
+}
